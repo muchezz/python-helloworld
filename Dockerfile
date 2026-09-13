1fe031ad12322c38b97a3a7733633a13ab271533
Phase6B Acceptance <acceptance@njiracloud.local>
2026-09-13 22:54:40 +0300

    Phase 6B acceptance fixture: Flask

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.8
-LABEL maintainer = "Mucheru"
-
-COPY . /app
-WORKDIR /app
-RUN pip install -r requirements.txt
-
-#Commands to run the container
-CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,39 +1,10 @@
 from flask import Flask
-from flask import json
-import logging
 
 app = Flask(__name__)
 
-@app.route('/status')
-def healthcheck():
-    response = app.response_class(
-            response=json.dumps({"result":"OK - healthy"}),
-            status=200,
-            mimetype='application/json'
-    )
-
-    app.logger.info('Status request successfull')
-    return response
-
-@app.route('/metrics')
-def metrics():
-    response = app.response_class(
-            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
-            status=200,
-            mimetype='application/json'
-    )
-
-    app.logger.info('Metrics request successfull')
-    return response
-
 @app.route("/")
 def hello():
-    app.logger.info('Main request successfull')
-
-    return "Hello World!"
+    return "Phase 6B Flask acceptance", 200
 
 if __name__ == "__main__":
-    ## stream logs to a file
-    logging.basicConfig(filename='app.log',level=logging.DEBUG)
-    
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
